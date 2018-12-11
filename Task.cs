b7400f5b5d57f98e6c11dddb2323e3dd5d2bb859
AbbarYassine <abbaryassin123@gmail.com>
2018-12-11 23:08:51 +0100

    fixing merge conflicts.

--- Task.cs
+++ Task.cs
@@ -1,69 +1,123 @@
+/*
+##### REVERB Project ######## Polytech Sorbonne - 2018/2019 #########
+Supervised by : François PÊCHEUX 
+Realised by : Yassine ABBAR - Aurélien ABEL - Fatine BENTIRES ALJ - Geng REN - Alexia ZOUNIAS-SIRABELLA
+*/
+
 using System;
 using System.Text;
 
 namespace class_task
 {
-	public class Task
-	{
 
-		public enum Level {Washing_machine, TV, Oven_kitchen, Tool_Kitchen, Tool_bathroom, Heating, Light};
+  public enum Level {Washing_machine, TV, Oven_kitchen, Tool_Kitchen, Tool_bathroom, Heating, Light};
 
-		// title of the task (number that will corresponds to the task)
-		public Level _title;//(Level)(new Random()).Next(0, 7); - j'ai mis à Washing_machine pour faire un test
+  public class Task
+  {
+
+    // title of the task (number that will corresponds to the task)
+    public Level _title;//(Level)(new Random()).Next(0, 7); - j'ai mis à Washing_machine pour faire un test
     public Level set_title(int number){ _title = (Level)(number); return _title;}
-    // consumption of the task for the duration
-		float consumption;
-		// duration of the task
-		DateTime begin_time;
-    DateTime end_time;
-		//points given to a task
-		public int points; 
 
-    public Task(int lvl, DateTime begin_time_task, int duration)
+    public string creator {get; private set;}
+    // consumption of the task for the duration
+    public float consumption {get; private set;}
+    // duration of the task
+    public DateTime begin_time {get; private set;}
+    public float duration {get; private set;}
+    public DateTime end_time {get; private set;}
+    //points given to a task
+    public int points; 
+
+    public Task(int lvl, DateTime begin_time_task, float duration, string name)
     {
       _title = (Level)(lvl);
-      //Console.WriteLine(_title);
+      // Console.WriteLine(_title);
       begin_time = begin_time_task;
+      duration = duration;
       end_time = begin_time.AddMinutes(duration);
       consumption = calculate_consumption(duration);
+      creator = name;
+      points_attribution();
     }
 
-
-    public float calculate_consumption(int duration)
+    public void recompute_time(double time)
     {
-      float consumption = 1;
-      return consumption;
+
+      begin_time = begin_time.AddMinutes(time);
+      end_time = begin_time.AddMinutes(duration);
     }
 
-		public void points_attribution()
-		{
-			switch (_title)
-      		{
-          		case Level.Washing_machine:
-              		points = 1; 
-              		break;
-          		case Level.TV:
-              		points = 2;
-              		break;
-          		case Level.Oven_kitchen:
-              		points = 3;
-              		break;
-          		case Level.Tool_Kitchen:
-              		points = 4;
-              		break;              		              	
-          		case Level.Tool_bathroom:
-              		points = 5;
-              		break;       
-          		case Level.Heating:
-              		points = 6;
-              		break;  
-          		case Level.Light:
-              		points = 7;
-              		break;               		            		
-          		default:
-              		points = 0;
-              		break;
-      		}	
-		}
-	}
+    public float calculate_consumption(float duration)
+    {
+
+      switch (_title)
+          {
+              case Level.Washing_machine:
+                  return 470/60 * duration;
+                  
+              case Level.TV:
+                  return 100/60 * duration;
+                  
+              case Level.Oven_kitchen:
+                  return 1200/60 * duration;
+                  
+              case Level.Tool_Kitchen:
+                  return 300/60 * duration;
+                                                    
+              case Level.Tool_bathroom:
+                  return 800/60 * duration;
+                         
+              case Level.Heating:
+                  return 1000/60 * duration; // arbitraire
+                    
+              case Level.Light:
+                  return 40/60 * duration;
+                                                    
+              default:
+                  return duration;
+                  
+          }
+    }
+
+
+    public void points_attribution()
+    {
+      switch (_title)
+          {
+              case Level.Washing_machine:
+                  points = 1; 
+                  break;
+              case Level.TV:
+                  points = 2;
+                  break;
+              case Level.Oven_kitchen:
+                  points = 3;
+                  break;
+              case Level.Tool_Kitchen:
+                  points = 4;
+                  break;                                  
+              case Level.Tool_bathroom:
+                  points = 5;
+                  break;       
+              case Level.Heating:
+                  points = 6;
+                  break;  
+              case Level.Light:
+                  points = 7;
+                  break;                                  
+              default:
+                  points = 0;
+                  break;
+          } 
+    }
+
+
+    public override string ToString()
+    {
+      return "Creator: "+creator+"\nPriority: "+points+"\nConsumption: " +consumption + "\nStarting time: " + begin_time;
+    }
+
+
+  }
 }
--- person.cs
+++ person.cs
@@ -14,7 +14,7 @@ namespace class_person
 		private static string[] survey = File.ReadAllLines("Data/SondageHabitudes.csv");
 		public string name {get; private set;}
 		public string[] habit {get; private set;}
-		public List<Task> _task { get; private set;}
+		public List<Task> list_tache { get; private set;}
 		private _figures _figure;
 
 
@@ -40,19 +40,53 @@ namespace class_person
 			double tv_prob_18_21 = 0.5335689046;
 			double tv_prob_21_0 = 0.5441696113;
 
-			// double pc_prob_0_6 = 0.09187279152;
-			// double pc_prob_6_9 = 0.2897526502;
-			// double pc_prob_9_12 = 0.4310954064;
-			// double pc_prob_12_15 = 0.4664310954;
-			// double pc_prob_15_18 = 0.5477031802;
-			// double pc_prob_18_21 = 0.7703180212;
-			// double pc_prob_21_0 = 0.6643109541;
+			double pc_prob_0_6 = 0.09187279152;
+			double pc_prob_6_9 = 0.2897526502;
+			double pc_prob_9_12 = 0.4310954064;
+			double pc_prob_12_15 = 0.4664310954;
+			double pc_prob_15_18 = 0.5477031802;
+			double pc_prob_18_21 = 0.7703180212;
+			double pc_prob_21_0 = 0.6643109541;
+
+		//public enum Level {Washing_machine, TV, Oven_kitchen, Tool_Kitchen, Tool_bathroom, Heating, Light};
+
+			double Ovenkitchen_prob_0_6 = 0.0;
+			double Ovenkitchen_prob_6_9 = 0.0353356890459364;
+			double Ovenkitchen_prob_9_12 = 0.03886925795053;
+			double Ovenkitchen_prob_12_15 = 0.127208480565371;
+			double Ovenkitchen_prob_15_18 = 0.0106007067137809;
+			double Ovenkitchen_prob_18_21 = 0.787985865724382;
+			double Ovenkitchen_prob_21_0 = 0.0989399293286219;
+
+			double ToolKitchen_prob_0_6 = 0.0;
+			double ToolKitchen_prob_6_9 = 0.664310954063604;
+			double ToolKitchen_prob_9_12 = 0.148409893992933;
+			double ToolKitchen_prob_12_15 = 0.261484098939929;
+			double ToolKitchen_prob_15_18 = 0.102473498233216;
+			double ToolKitchen_prob_18_21 = 0.65017667844523;
+			double ToolKitchen_prob_21_0 = 0.212014134275618;
+
+/*
+			double Heating_prob_0_6 = 0.09187279152;
+			double Heating_prob_6_9 = 0.2897526502;
+			double Heating_prob_9_12 = 0.4310954064;
+			double Heating_prob_12_15 = 0.4664310954;
+			double Heating_prob_15_18 = 0.5477031802;
+			double Heating_prob_18_21 = 0.7703180212;
+			double Heating_prob_21_0 = 0.6643109541;
+			double Light_prob_0_6 = 0.09187279152;
+			double Light_prob_6_9 = 0.2897526502;
+			double Light_prob_9_12 = 0.4310954064;
+			double Light_prob_12_15 = 0.4664310954;
+			double Light_prob_15_18 = 0.5477031802;
+			double Light_prob_18_21 = 0.7703180212;
+			double Light_prob_21_0 = 0.6643109541;
+*/
+
+			List<Task> created_task = new List<Task>(); // list to be returned by action in order to be stored in smartgrid
 
 			int hour = Int32.Parse(current_time.ToString().Substring(11,2));
-
-			// List where all the task of the person are stored
-			List<Task> list_tache = new List<Task>();
-
+			
 			if(hour == 0) //new day so we change the random_line
 			{
 				int random_line = new Random().Next(1, 284);
@@ -70,11 +104,41 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[6]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[6])));
-						Console.WriteLine(name+" turn on tv for "+habit[6]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[6]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[6]);
 					}
 				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_0_6)
+				{	
+					if(Int32.Parse(habit[13]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[13]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[13]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_0_6)
+				{	
+					if(Int32.Parse(habit[20]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[20]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[20]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_0_6)
+				{	
+					if(Int32.Parse(habit[27]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[27]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[27]);
+					}
+				}
+
 			}
 			else if (hour >= 6 && hour < 9)
 			{
@@ -83,9 +147,38 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[0]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[0])));
-						Console.WriteLine(name+" turn on tv for "+habit[0]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[0]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[0]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_6_9)
+				{	
+					if(Int32.Parse(habit[7]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[7]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[7]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_6_9)
+				{	
+					if(Int32.Parse(habit[14]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[14]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[14]);
+					}
+				}				
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_6_9)
+				{	
+					if(Int32.Parse(habit[21]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[21]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[21]);
 					}
 				}
 			}
@@ -96,11 +189,40 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[1]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[1])));
-						Console.WriteLine(name+" turn on tv for "+habit[1]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[1]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[1]);
 					}
 				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_9_12)
+				{	
+					if(Int32.Parse(habit[8]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[8]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[8]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_9_12)
+				{	
+					if(Int32.Parse(habit[15]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[15]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[15]);
+					}
+				}	
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_9_12)
+				{	
+					if(Int32.Parse(habit[22]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[22]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[22]);
+					}
+				}							
 			}
 			else if (hour >= 12 && hour < 15)
 			{
@@ -109,11 +231,40 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[2]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[2])));
-						Console.WriteLine(name+" turn on tv for "+habit[2]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[2]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[2]);
 					}
 				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_12_15)
+				{	
+					if(Int32.Parse(habit[9]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[9]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[9]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_12_15)
+				{	
+					if(Int32.Parse(habit[16]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[16]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[16]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_12_15)
+				{	
+					if(Int32.Parse(habit[23]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[23]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[23]);
+					}
+				}	
 			}
 			else if (hour >= 15 && hour < 18)
 			{
@@ -122,11 +273,40 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[3]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[3])));
-						Console.WriteLine(name+" turn on tv for "+habit[3]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[3]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[3]);
 					}
 				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_15_18)
+				{	
+					if(Int32.Parse(habit[10]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[10]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[10]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_15_18)
+				{	
+					if(Int32.Parse(habit[17]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[17]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[17]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_15_18)
+				{	
+					if(Int32.Parse(habit[24]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[24]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[24]);
+					}
+				}					
 			}
 			else if (hour >= 18 && hour < 21)
 			{
@@ -135,11 +315,40 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[4]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[4])));
-						Console.WriteLine(name+" turn on tv for "+habit[4]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[4]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[4]);
 					}
 				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_18_21)
+				{	
+					if(Int32.Parse(habit[11]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[11]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[11]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_18_21)
+				{	
+					if(Int32.Parse(habit[18]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[18]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[18]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_18_21)
+				{	
+					if(Int32.Parse(habit[25]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[25]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[25]);
+					}
+				}	
 			}
 			else if (hour >= 21)
 			{
@@ -148,98 +357,76 @@ namespace class_person
 				{	
 					if(Int32.Parse(habit[5]) != 0)
 					{
-						// turn on tv
-						list_tache.Add(new Task(1,current_time,Int32.Parse(habit[5])));
-						Console.WriteLine(name+" turn on tv for "+habit[5]);
+						created_task.Add(new Task(1,current_time,Int32.Parse(habit[5]),name));
+						//Console.WriteLine(name+" turns on tv for "+habit[5]);
 					}
 				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < pc_prob_21_0)
+				{	
+					if(Int32.Parse(habit[12]) != 0)
+					{
+						created_task.Add(new Task(2,current_time,Int32.Parse(habit[12]),name));
+						//Console.WriteLine(name+" uses the pc for "+habit[12]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < Ovenkitchen_prob_21_0)
+				{	
+					if(Int32.Parse(habit[19]) != 0)
+					{
+						created_task.Add(new Task(3,current_time,Int32.Parse(habit[19]),name));
+						//Console.WriteLine(name+" turns on the oven for "+habit[19]);
+					}
+				}
+
+				event_prob = new Random().NextDouble();
+				if(event_prob < ToolKitchen_prob_21_0)
+				{	
+					if(Int32.Parse(habit[26]) != 0)
+					{
+						created_task.Add(new Task(4,current_time,Int32.Parse(habit[26]),name));
+						//Console.WriteLine(name+" uses the kitchen for "+habit[26]);
+					}
+				}	
 			}
 
-			return list_tache;
-
-			// for( int i = 1; i < number_tasks; i ++){
-			// foreach (String l in survey) //we check the data base 
-			// {
-   //  			if (nbiter == 0)
-   //  			{
- 		// 			Console.WriteLine("Line:");
-   //      			Console.WriteLine(survey[random_line]); 
-			// 		column = survey[random_line].Split(',');
-
-			// 		int random_line_print = random_line + 1; 
-   //      			Console.WriteLine("line selected : " + random_line_print);
-
-   //      			Console.WriteLine("l1");
-   //      			for(iterateur = 0; iterateur < 7 ; iterateur++) //columns B - H - Jour travaillé - Jeux vidéos + TV
-   //      			{
- 		// 				Console.WriteLine(column[iterateur]); 
- 		// 				task.set_title(1); //TV
-   //      			}
-
-   //      			Console.WriteLine("l2");
-   //      			for(iterateur = 7; iterateur < 14; iterateur++) //I - O -  Jour travaillé - Ordinateur/Tablette
-   //      			{
- 		// 				Console.WriteLine(column[iterateur]);
- 		// 				task.set_title(3);  //Computer
-   //      			}
-
-   //      			Console.WriteLine("l3");
-   //      			for(iterateur = 14; iterateur < 21; iterateur++)  //P - V -  Jour travaillé - Four 
-   //      			{	
-   //      				Console.WriteLine(column[iterateur]); 
-   //      				task.set_title(2); //Kitchen
-   //      			}
-
-			// 		Console.WriteLine("l4");
-   //      			for(iterateur = 21; iterateur < 28; iterateur++) //W - AC -  Jour travaillé - Appareil cuisine courte durée (machine café...)
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      				task.set_title(5); //Cook
-   //      			}
-
-   //      			Console.WriteLine("l5");
-   //      			for(iterateur = 28; iterateur < 35; iterateur++) //AD - AJ -  Jour travaillé - Appareil salle de bain courte durée (lisseur...)
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      				task.set_title(4); //Tool_bathroom
-   //      			}
-
-   //      			Console.WriteLine("l6");
-   //      			for(iterateur = 35; iterateur < 42; iterateur++) //AK - AQ - Jour non travaillé - Jeux vidéos + TV
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      			}
-
-			// 		Console.WriteLine("l7");
-   //      			for(iterateur = 42; iterateur < 49; iterateur++) //AR - AX - Jour non travaillé - Ordinateur/Tablette
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      			}
-
-   //      			Console.WriteLine("l8");
-   //      			for(iterateur = 49; iterateur < 56 ; iterateur++) //AY - BE - Jour non travaillé - Four
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      			}
-
-   //      			Console.WriteLine("l9");
-   //      			for(iterateur = 56; iterateur < 63 ; iterateur++) //BF - BL - Jour non travaillé - Appareil cuisine courte durée (machine café...)
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      			}
-
-   //      			Console.WriteLine("l10");
-   //      			for(iterateur = 63; iterateur < 70 ; iterateur++) //BM - BS - Jour non travaillé - Appareil salle de bain courte durée (lisseur...)
-   //      			{
-   //      				Console.WriteLine(column[iterateur]);
-   //      			}
-
-   //      			nbiter = 1;  //to have only one line 
-   //  			}
-			// }//end foreach 
- 		// }//end for 
+			list_tache.AddRange(created_task);
+			return created_task;
 		}
-	
+
+		public double probability(int column)
+		{
+			int counter = 0; 
+			string[] habit2; 
+			double proba = 0.0; 
+			//Console.WriteLine("#####################################");
+			//Console.WriteLine("column : " + column);
+		
+			for(int line = 1; line <283; line++) //we read all the lines 
+			{
+				habit2 = survey[line].Split(",");
+				////Console.WriteLine("habit2[column] : "+ habit2[column]+ ", column :" + column);
+				
+				if(Int32.Parse(habit2[column]) != 0)
+				{
+					////Console.WriteLine("habit2 ! = 0 : "+ habit2[column]+ ", column :" + column);
+					counter+=1; 
+				} 
+			}
+
+			////Console.WriteLine("counter : " + counter);
+			proba = (double) counter/ (double) 283; 
+			return proba; 
+		}
+
+		public void Chosen_Task(Person person, int lvl, DateTime current_time, int duration) //to add a new task into the list 
+    	{
+      		list_tache.Add(new Task(lvl,current_time, duration, name)); 
+      		//Console.WriteLine("Task, lvl : " + lvl + ", current_time : " + current_time + ", duration : " + duration); 
+    	}	
 		public override string ToString()
 		{
 			return name;
