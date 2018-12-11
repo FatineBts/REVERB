dacf07c7387cbf3c0de2602eca67b5699a73e759
AbbarYassine <abbaryassin123@gmail.com>
2018-12-11 19:12:46 +0100

    House: Add some methods.

--- house.cs
+++ house.cs
@@ -17,10 +17,12 @@ namespace class_house
 		float _solar_panel_battery;
 		private RSACryptoServiceProvider key_pair;
 		public RSAParameters public_key {get; private set;}
+		public enum Season = {winter, autumn, spring, summer}; // summer <=> 4 kw, spring <=> 3 kw, autumn <=> 2 kw ,winter <=> 1 kw 
+		private Season _season;
 
-		public House(float production)
+		public House(Season season)
 		{
-			_solar_panel_battery = production;
+			_season = season; 
 			_family = new List<Person>();
 			// Create a key pair
 			key_pair = new RSACryptoServiceProvider();
@@ -28,10 +30,34 @@ namespace class_house
 			public_key = key_pair.ExportParameters(false);
 			//Console.WriteLine(survey);
 		}
-
-		public void AddFamilyMember()
+		public ChangeSeason(Season season)
 		{
-			_family.Add(CreateFirstBlock());
+		 	_season = season;
+		}
+
+		public void AddProductionPerDay()
+		{
+			switch(_season)
+			{
+				case _season.winter:
+					_solar_panel_battery = 1000;
+					break;
+				case _season.autumn:
+					_solar_panel_battery = 2000;
+					break;
+				case _season.spring:
+					_solar_panel_battery = 3000;
+					break;
+				case _season.summer:
+					_solar_panel_battery = 4000;
+					break;			
+			}
+
+		}
+
+		public void AddFamilyMember(Person NewPerson)
+		{
+			_family.Add(NewPerson);
 		}
 
 		public string sign(Person dest, int amount)
@@ -46,6 +72,11 @@ namespace class_house
 			byte[] data_to_be_verify = Encoding.ASCII.GetBytes(this.ToString() + dest.ToString() + amount.ToString());
 
 			return key_pair.VerifyData(data_to_be_verify, new SHA256CryptoServiceProvider(), Convert.FromBase64String(sign_hash));
+		}
+
+		public override string ToString()
+		{
+			return name+Encoding.UTF8.GetString(public_key.Modulus);
 		}
 		
 		
